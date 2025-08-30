24f6440b66ef32971f48139d65a43eebef85925a
yimgg <yanjunming2000@163.com>
2025-08-30 14:38:15 +0800

    更改配置文件

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to aoping's blog"/>
+    <HelloWorld msg="Welcome to Yim's blog"/>
   </div>
 </template>
 
